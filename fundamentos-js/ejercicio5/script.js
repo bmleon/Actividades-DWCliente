6821c55efe3c6be267adba0771cef7539bc8e8e3
Belén María León <75305764+bmleon@users.noreply.github.com>
2025-10-23 20:09:33 +0200

    Delete fundamentos-js/ejercicio5 directory

--- fundamentos-js/ejercicio5/script.js
+++
@@ -1,8 +0,0 @@
-let numero = Number(prompt("Ingresa un número:"));
-let resultado = document.getElementById("resultado");
-
-if (numero % 2 == 0) {
-    resultado.textContent = ("El número " + numero + " es par");
-} else {
-    resultado.textContent = ("El número " + numero + " es impar");
-}
